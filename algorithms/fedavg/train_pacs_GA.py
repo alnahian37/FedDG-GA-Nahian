e4599ec9e545b2b4f1492678cca9258659f4b2b8
alnahian37 <nahian.buet11@gmail.com>
2024-02-29 17:12:26 -0500

    version runs

--- algorithms/fedavg/train_pacs_GA.py
+++ algorithms/fedavg/train_pacs_GA.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 import argparse
 from utils.log_utils import *
 from torch.utils.tensorboard.writer import SummaryWriter
--- configs/default.py
+++ configs/default.py
@@ -3,7 +3,8 @@ domainNet_domain_list = ['c', 'i', 'p', 'q', 'r', 's']
 officehome_domain_list = ['p', 'a', 'c', 'r']
 terra_incognita_list = ['100', '38', '43', '46']
 
-log_path = ''
-pacs_path = ''
+log_path = './log'
+pacs_path = './data/PACS_Dataset'
 officehome_path = ''
 terrainc_path = ''
+log_count_path = './log/'
